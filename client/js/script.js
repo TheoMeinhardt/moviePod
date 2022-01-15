8b439c3e481acde8eb7752d9d0ddee50ce8b1415
kmatura <matura.k04@htlwienwest.at>
2022-01-15 18:12:42 +0100

    Back to b4197b6

--- client/js/script.js
+++ client/js/script.js
@@ -1,28 +1,20 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 const { createApp } = Vue;
 
 var moviePod = ({
     data() {
         return{
-            res: "",  
-            movieSearch: "",  
+            movie: '',
+            searchTitle: '',
         };
     },
     methods: {
-        async searchMovie(){
-            let searchTitle;
-            if(this.movieSearch.includes(" ")){
-                searchTitle = this.movieSearch.replace(/ /g,"+");
-            }
-            let response = await axios.get(`http://localhost:3000/getmovie/${searchTitle}`);
-            this.res = response.data;
+        async getMovie(){
+            this.searchTitle.replace(/\s/g, "+");
+			const {data} = await axios.get(`http://localhost:3000/getMovie/${this.searchTitle}`);
+            console.log(data);
+            this.movie = data;
         }
     }
 })
 
 createApp(moviePod).mount('#app');
-=======
->>>>>>> parent of b4197b6 (Movie Search implemented)
-=======
->>>>>>> parent of b4197b6 (Movie Search implemented)
--- server/controllers/moviesPOST.js
+++ server/controllers/moviesPOST.js
@@ -1,6 +1,7 @@
 import Pool from 'pg-pool';
 import log from 'loglevel';
 import { getUserId } from './users.js';
+import { checkMovieInPersonalList } from './moviesDELETE.js';
 import { config as dotenvConfig } from 'dotenv';
 
 dotenvConfig();
@@ -12,12 +13,15 @@ async function addMovieToPersonalList(req, res) {
     const { username, movieTitle } = req.body;
     const userid = await getUserId(username);
 
+    console.log(userid);
     if (isNaN(userid)) {
-      throw new Error('user not found');
+      res.status(400).send('user does not exist');
+      throw new Error('user does not exist');
     }
 
     if (await checkMatchDuplicate(userid, movieTitle)) {
-      throw new Error('Movie already in list');
+      res.status(400).send('movie already in list');
+      throw new Error('movie already in list');
     }
 
     await addMovieToDB(movieTitle);
@@ -62,4 +66,25 @@ async function checkMatchDuplicate(user, title) {
   }
 }
 
-export { addMovieToPersonalList };
+async function rateMovie(req, res) {
+  const { username, movieTitle, rating } = req.body;
+
+  try {
+    if (!(await checkMovieInPersonalList(username, movieTitle))) {
+      res.status(400).send('movie not in list');
+      throw new Error('Movie not in personal list');
+    }
+
+    const text = 'UPDATE movielist SET personalRating = $1 WHERE userid = $2 AND movieTitle = $3';
+    const params = [rating, await getUserId(username), movieTitle];
+
+    await pool.query(text, params);
+
+    res.status(200).send('OK');
+  } catch (err) {
+    log.error(err);
+    res.status(500).send(err);
+  }
+}
+
+export { addMovieToPersonalList, rateMovie };
